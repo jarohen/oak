652061ebc76ff9bd7856db9f1d180faf5839cd87
James Henderson <james@jarohen.me.uk>
2016-09-08 09:44:42 +0100

    some renaming, and correcting examples

--- src/oak/core.cljs
+++ src/oak/core.cljs
@@ -9,7 +9,12 @@
 (defn update-db [{:keys [app db] :as ctx} f & args]
   (apply update ctx :db f args))
 
-(defn narrow [{:keys [app db] :as ctx} & path]
+(defn update-in-ctx [{:keys [app] :as ctx} app-path f & args]
+  (let [{new-app :app, :keys [db]} (f (apply narrow ctx app-path))]
+    {:app (assoc-in app app-path new-app)
+     :db db}))
+
+(defn get-in-ctx [{:keys [app db] :as ctx} path]
   (update ctx :app get-in path))
 
 (defn ev
@@ -40,17 +45,17 @@
 
 (defn counter-handle [ctx ev]
   (case (:oak/type ev)
-    :inc-clicked (update-app ctx :count inc)))
+    :inc-clicked (o/update-app ctx :count inc)))
 
 (defn handle [ctx ev]
   (case (:oak/type ev)
-    :counter (counter-handle (-> ctx (o/narrow :counters idx)) (:oak/inner ev))))
+    :counter (update-in-ctx ctx [:counters idx] counter-handle (:oak/inner ev))))
 
 (defn view [ctx]
   (o/h
    [:div
     (for [idx (range 2)]
-      (-> (o/h [sub-view (-> ctx (o/narrow :counters idx))])
+      (-> (o/h [sub-view (o/get-in-ctx ctx [:counters idx])])
           (o/wrap-ev :counter {:idx idx})))]))
 
 (o/start! {:$el (js/document.getElementById "app")
