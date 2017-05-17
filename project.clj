0f1ea824ff7f12e6d8c0ca1337add70ec2bcc760
James Henderson <james@jarohen.me.uk>
2017-05-17 09:00:08 +0100

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject jarohen/oak "0.0.1-SNAPSHOT"
+  :description "A ClojureScript library to structure single-page apps - taking inspiration from the Elm Architecture"
+  :url "https://github.com/jarohen/oak"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.3.442"]])
